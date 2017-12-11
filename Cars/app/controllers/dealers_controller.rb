22e1972076638aa3ec387307dca5ba68bad0ffc4
marco acosta <marcucho14@hotmail.com>
2017-12-11 10:29:49 -0500

    made create and edit work 50%

--- Cars/app/controllers/dealers_controller.rb
+++ Cars/app/controllers/dealers_controller.rb
@@ -5,6 +5,38 @@ end
 
 def show
   @dealer = Dealer.find(params[:id])
-  @id = params[:id]
 end
+
+def new
+  @dealer =Dealer.new
+end
+def create
+  @dealer = Dealer.new(dealer_params)
+  if @dealer.valid?
+    @dealer.save
+    redirect_to @dealer
+  else
+    flash[:dealer] = @dealer.errors.messages
+    redirect_back fallback_location: edit_dealer_path(@dealer)
+  end
+end
+
+def edit
+end
+
+def update
+    if @dealer.update(dealer_params)
+      redirect_to @dealer
+    else
+      redirect_back fallback_location: edit_dealer_path(@dealer)
+    end
+  end
+
+  def destroy
+    if @dealer.destroy
+      redirect_to makes_path
+    else
+      redirect_back fallback_location: @dealer
+    end
+  end
 end
--- Cars/app/controllers/makes_controller.rb
+++ Cars/app/controllers/makes_controller.rb
@@ -6,4 +6,38 @@ end
 def show
   @make = Make.find(params[:id])
 end
+
+def new
+  @make =Make.new
+end
+
+def create
+  @make = Make.new(make_params)
+  if @make.valid?
+    @make.save
+    redirect_to @make
+  else
+    flash[:make] = @make.errors.messages
+    redirect_back fallback_location: edit_make_path(@make)
+  end
+end
+
+def edit
+end
+
+def update
+    if @make.update(make_params)
+      redirect_to @make
+    else
+      redirect_back fallback_location: edit_make_path(@make)
+    end
+  end
+
+  def destroy
+    if @make.destroy
+      redirect_to make_path
+    else
+      redirect_back fallback_location: @make
+    end
+  end
 end
